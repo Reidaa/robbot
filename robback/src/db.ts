f6606b574fafd67d6b0554b54fcc84ccef64e35d
Thomas Kemkemian <thomas.kemkemian@gmail.com>
2024-07-06 05:13:02 +0200

    use drizzle basic

---
+++ robback/src/db.ts
@@ -0,0 +1,6 @@
+import {neon} from '@neondatabase/serverless';
+import {drizzle} from 'drizzle-orm/neon-http';
+import * as schema from 'drizzle/schema';
+
+const client = neon(process.env.DATABASE_URL as string);
+export const db = drizzle(client, {schema: schema, logger: true});
--- robback/src/index.ts
+++ robback/src/index.ts
@@ -1,5 +1,13 @@
-const main = () => {
+import 'dotenv/config';
+
+import {db} from './db';
+
+async function main() {
   console.log('hello Node.js and Typescript world :]');
-};
+
+  const manga = await db.query.MangaTable.findFirst();
+
+  console.log(manga);
+}
 
 main();
