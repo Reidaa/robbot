cf43bd03f4dbccccffc948edd8cac2108eb19c92
Thomas Kemkemian <thomas.kemkemian@gmail.com>
2024-07-06 16:40:38 +0200

    use server_id as bigInt to keep precision

--- robback/README.md
+++ robback/README.md
@@ -3,3 +3,8 @@
 > back this robbot up
 
 [![Code Style: Google](https://img.shields.io/badge/code%20style-google-blueviolet.svg)](https://github.com/google/gts)
+
+## Idée(s)
+
+* Utiliser MangaDex et son API afin d'éviter de nombreux problemes de normalisation vis a vis des mangas et leurs titres ...
+  * CONS: peut etre pas mis a jour tres rapidement, pour un MVP ca fera l'affaire
--- robback/src/index.ts
+++ robback/src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 
 import {db} from './db';
+import {MangaServerTable} from 'drizzle/schema';
+import {eq} from 'drizzle-orm';
 
 async function main() {
   console.log('hello Node.js and Typescript world :]');
@@ -8,6 +10,13 @@ async function main() {
   const manga = await db.query.MangaTable.findFirst();
 
   console.log(manga);
+
+  const subs = await db
+    .select({id: MangaServerTable.serverId})
+    .from(MangaServerTable)
+    .where(eq(MangaServerTable.serverId, BigInt('1082820297876574239')));
+
+  console.log(subs);
 }
 
 main();
