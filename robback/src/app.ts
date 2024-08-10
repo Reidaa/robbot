0ec1ea80a9fd102d6732750816d4494759a6e02c
Thomas KEMKEMIAN <thomas.kemkemian@gmail.com>
2024-08-10 04:53:40 +0200

    minor edit

--- robback/src/app.ts
+++ robback/src/app.ts
@@ -4,10 +4,7 @@ import {FastifyInstance, FastifyPluginOptions} from 'fastify';
 import AutoLoad from '@fastify/autoload';
 import Sensible from '@fastify/sensible';
 
-export default async function (
-  fastify: FastifyInstance,
-  opts: FastifyPluginOptions
-) {
+async function app(fastify: FastifyInstance, opts: FastifyPluginOptions) {
   await fastify.register(Sensible);
   await fastify.register(AutoLoad, {
     dir: join(resolve(__dirname), 'routes'),
@@ -15,3 +12,5 @@ export default async function (
     options: Object.assign({}, opts),
   });
 }
+
+export default app;
